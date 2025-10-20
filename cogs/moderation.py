b45208ae869cece0ee0894666d5d045013a1df48
Keriptos <jasondrake058@gmail.com>
2025-10-20 20:10:08 +0700

    Added a reload cog function so the bot doesn't need to reboot to apply changes to commands

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -4,6 +4,7 @@ from discord import app_commands
 from discord.ext import commands
 
 #Other Imports
+import os
 import time
 
 GUILD_ID = discord.Object(id = 1391372922219659435) #This is my server's ID, and I'm only gonna use it for my server
@@ -48,6 +49,7 @@ class Moderation(commands.Cog) :
             await interaction.response.send_message(f"An error has occured: {error}", ephemeral=True)
             print(f"An error has occured when a user tried to purge messages: {error}\n")
 
+
     @app_commands.command(name="sync", description="(ADMIN ONLY) Syncs the app commands")
     @app_commands.checks.has_role("Botministrator")
     async def syncCommands(self, interaction: discord.Interaction):
@@ -73,6 +75,46 @@ class Moderation(commands.Cog) :
         else :
             print(f"An error has occured when a user tried to sync commands: {error}\n")
             await interaction.response.send_message(f"An error has occured: {error}", ephemeral=True)
+
+    
+    async def cog_autocomplete(self, interaction: discord.Interaction, current: str):
+        cogFiles = []
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                extensionName = f"cogs.{filename[:-3]}" # ":-3" removes 3 characters (.py) starting from behind the filename
+                cogFiles.append(extensionName)
+
+        return [ # This is the template from the discordpy doc. I only changed the variable name. Don't dare touch it
+            app_commands.Choice(name = file, value = file)
+            for file in cogFiles if current.lower() in file.lower()
+        ][:25] # Discord only allows max of 25 items
+
+
+    @app_commands.command(name = "reload", description= "(ADMIN ONLY) Reloads a cog file")
+    @app_commands.autocomplete(cogfile = cog_autocomplete) 
+    @app_commands.checks.has_role("Botministrator")
+    async def reload(self, interaction: discord.Interaction, cogfile: str):
+        commandStartTime = time.perf_counter()
+        print(f"{interaction.user.name} is trying to reload a cog file")
+
+        try:
+            await self.bot.reload_extension(f"{cogfile}")
+            await interaction.response.send_message(f"Reloaded {cogfile} succesfully!", ephemeral=True)
+        except Exception as error:
+             await interaction.response.send_message(f"Failed to reload {cogfile}, {error}!", ephemeral=True)
+
+        commandEndTime = time.perf_counter()
+        print(f"Ended cogReload command in {commandEndTime - commandStartTime:0.4f} seconds\n")
+
+    @reload.error
+    async def reloadError(self, interaction: discord.Interaction, error):
+        if isinstance(error, app_commands.errors.MissingRole) :
+            print(f"{interaction.user} tried to reload a cog file without the required role\n")
+            await interaction.response.send_message("You don't have the required role to use this command❗", ephemeral=True)
+        else :
+            print(f"An error has occured when a user tried to sync commands: {error}\n")
+            await interaction.response.send_message(f"An error has occured: {error}", ephemeral=True)
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Moderation(bot), guild=GUILD_ID)
