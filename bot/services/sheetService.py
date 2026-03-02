edbae5257f8915dcc2107e86a05ec74b59603af9
Keriptos <jasondrake058@gmail.com>
2026-03-02 22:33:17 +0700

    Added year column caching, so the only API call that's not lazy loaded/cached is the batch update

--- bot/services/sheetService.py
+++ bot/services/sheetService.py
@@ -8,6 +8,7 @@ class SheetService:
     def __init__(self):
         self.sheet: Spreadsheet | None = None        
         self.worksheets: dict[str, Worksheet] = {}
+        self.year_column_cache: dict[str, list[int | str | float | None]] = {}
         
     def get_sheet_client(self) -> Spreadsheet:
         commandStartTime = time.perf_counter()
@@ -21,7 +22,7 @@ class SheetService:
         return self.sheet
 
     def load_worksheets(self) -> dict[str, Worksheet]:
-        if self.worksheets == {}:
+        if not self.worksheets:
             start = time.perf_counter()
             self.sheet = self.get_sheet_client()
             worksheets = self.sheet.worksheets()            
@@ -31,7 +32,6 @@ class SheetService:
             print(f"Loaded all worksheets in {end-start:.8f} seconds")
         return self.worksheets
 
-
     def get_worksheet(self, username) -> Worksheet:
         self.load_worksheets()
         try:
@@ -39,6 +39,17 @@ class SheetService:
         except KeyError:
             raise gspread.WorksheetNotFound(f"{username}'s worksheet not found. User should register first!")
         
-
-sheetManager = SheetService()
+    def load_year_columns(self, username) -> list[int | str | float | None]:
+        if not self.year_column_cache:                
+            worksheet = self.get_worksheet(username)              
+            start = time.perf_counter()
+            self.year_column_cache[username] = worksheet.col_values(4)
+            end = time.perf_counter()
+            print(f"Year column cache was set-up in {end - start:.4f} seconds")
+        return self.year_column_cache[username]
     
+    def get_year_column(self, username: str):
+        if username not in self.year_column_cache:
+            print(f"Didn't found {username} in year column cache. Setting up cache...")
+            self.load_year_columns(username)
+        return self.year_column_cache[username]
