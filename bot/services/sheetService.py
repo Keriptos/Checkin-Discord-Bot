0ef96873b2dd8aa97fe5af05db3c0850e696f642
Keriptos <jasondrake058@gmail.com>
2026-03-04 23:09:00 +0700

    Remove redudancy in year caching

--- bot/services/sheetService.py
+++ bot/services/sheetService.py
@@ -20,7 +20,8 @@ class SheetService:
             commandEndTime = time.perf_counter()
             print(f"Initialized sheet client in {commandEndTime - commandStartTime:.4f} seconds")
         return self.sheet
-        
+    
+    
     def force_load_worksheets(self) -> dict[str, Worksheet]:
         start = time.perf_counter()
         self.sheet = self.get_sheet_client()
@@ -41,23 +42,18 @@ class SheetService:
         except KeyError:
             raise gspread.WorksheetNotFound(f"{username}'s worksheet not found. User should register first!")
         
-
-    def load_year_columns(self, username) -> list[int | str | float | None]:
-        if not self.year_column_cache:  
+    
+    def get_year_column(self, username: str) -> list[int | str | float | None]:
+        if username not in self.year_column_cache:
+            print(f"Didn't found {username} in year column cache. Setting up cache...")
             worksheet = self.get_worksheet(username)
             start = time.perf_counter()
             self.year_column_cache[username] = worksheet.col_values(4)
             end = time.perf_counter()
             print(f"Year column cache was set-up in {end - start:.4f} seconds")
         return self.year_column_cache[username]
-    
-    def get_year_column(self, username: str):
-        if username not in self.year_column_cache:
-            print(f"Didn't found {username} in year column cache. Setting up cache...")
-            self.load_year_columns(username)
-        return self.year_column_cache[username]
 
 
 # To prevent making another class instance in any of the logic files, 
 # it's better to import the variable from this module
-sheetManager = SheetService() 
+sheetManager = SheetService()
