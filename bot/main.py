8757098ee5f88e3e6b31a6baa0a7b86e6bef3aea
Cryptoz <jasondrake058@gmail.com>
2026-03-26 19:31:46 +0700

    Deleted a bunch of whitespaces

--- bot/main.py
+++ bot/main.py
@@ -12,14 +12,10 @@ bot = commands.Bot(command_prefix = None,  intents = discord.Intents.default()) 
 
 @bot.event
 async def on_ready():
-    print("Bot Ready!") 
-    
-    
+    print("Bot Ready!")         
     # Sync commands to the bot
     try :        
-        _GUILD_ID = discord.Object(id = CFG.GUILD_ID) # This is my server's ID, and I'm only gonna use it for my server
-
-    
+        _GUILD_ID = discord.Object(id = CFG.GUILD_ID) # This is my server's ID, and I'm only gonna use it for my server    
         syncedCommands = await bot.tree.sync(guild= _GUILD_ID) # It'll return a list of commands that had been synced
         print(f"Synced {len(syncedCommands)} commands.\n")
         await bot.get_channel(1393987877599445115).send(f"@here Bot is online!") # Announce in the channel(check-ins-out) that bot is online
