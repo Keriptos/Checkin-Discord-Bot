a1ac3bf85c092a8e772c9ff5a544342d5d456c34
Cryptoz <jasondrake058@gmail.com>
2026-03-29 17:21:42 +0700

    Added support for multiple activities for the excuses menu

--- bot/cogs/excuses.py
+++ bot/cogs/excuses.py
@@ -19,8 +19,8 @@ class LabelsMenu(discord.ui.Select):
     """ A menu to use other labels to fill in the activity days.
         THIS WILL FILL EVERY ACTIVITY DAY
     """
-    def __init__(self, userID: str, activity: str):
-        self.chosenActivity = activity
+    def __init__(self, userID: str, activities: list):
+        self.chosenActivities: list = activities
         self.userID: str = userID
         self.usersData: dict = utls.loadJSON(CFG.USERS_FILE)
         try:
@@ -81,8 +81,15 @@ class LabelsMenu(discord.ui.Select):
     async def callback(self, interaction: discord.Interaction):
         commandStartTime = time.perf_counter()
         chosenExcuse: str = self.values[0] # User selected values will always be 1 element, which is a string        
-        print(f"{self.username}'s excuse for {self.chosenActivity}: {chosenExcuse}")
+        print(f"{self.username}'s excuse for {self.chosenActivities}: {chosenExcuse}")
         
+        try:
+            user = self.usersData[self.userID]
+        except KeyError:
+            print(f"{interaction.user} was not found in the local users file")
+            await interaction.followup.send(f"An error has occured!")
+            await interaction.followup.send(f"Your user profile was not found. If you had registered already, please DM the admin", ephemeral=True)
+            return 
         
         await interaction.response.defer()
         print("Going to Sheets")
@@ -90,23 +97,22 @@ class LabelsMenu(discord.ui.Select):
         worksheetID = worksheet.id
         print(f"Got {self.username}'s worksheet")
 
-        date = datetime.datetime.now()
-       
-        yearCell: dict = sheetManager.get_year_cell(self.user, date)
 
+        date = datetime.datetime.now()
+        yearCell: dict = sheetManager.get_year_cell(user, date)
         if self.userFormat == "Yearly":
             yearDivCell= None
             monthCell = sheetManager.get_month_cell(self.user, date, yearCell, yearDivCell)
 
-            # Get rowToFind & columnToFind. Decrement by 1 afterwards so that it's 0-indexed
-            rowToFind = (monthCell["row"] - 1) + date.day  # The first day is a row after monthRow. 
-            columnToFind = monthCell["col"] - 1 # Since there's only 1 activity, columnToFind is just monthColumn
+            # Values are decremeneted by 1, so that it's 0-indexed
+            rowToFind = (monthCell["row"] - 1) + date.day  # The first day is a row after monthRow.
+            columnToFind: list = [monthCell["col"] - 1] # Made into a list so that ExcuseReq could be consistent with 2+ activities
             
         else:
             yearDivCell= sheetManager.get_year_division_cell(yearCell, self.user, date)
             monthCell = sheetManager.get_month_cell(self.user, date, yearCell, yearDivCell)
 
-            # Get rowTofind & columnToFind. There's no need to decrement by 1
+            # There's no need to decrement by 1
             rowToFind = monthCell["row"] + date.day # The first day is 2 rows after monthRow. (0-indexed)                                           
 
             # Map the activity, offset columnToFind based on monthCell with baseIndex from the activity map
@@ -115,36 +121,39 @@ class LabelsMenu(discord.ui.Select):
                 for index, activity in enumerate(self.registeredActivity):
                     activityIndex[activity] = index
                 
-                if self.chosenActivity in activityIndex:
-                    baseIndex = activityIndex[self.chosenActivity]
-                    offset = baseIndex + monthCell["col"] - 1       
-                    columnToFind = offset
+                columnToFind: list = []
+                for activity in self.chosenActivities:
+                    if activity in activityIndex:
+                        baseIndex = activityIndex[activity]
+                        offset = baseIndex + monthCell["col"] - 1       
+                        columnToFind.append(offset)
             
             except Exception as error:
                 print(f"An error occured when getting rowToFind or columnToFind, {error}")
         
         # Request section
-        compiledRequests = [] 
-        ExcuseReq = { 
-            "requests": [
-                {
-                    "updateCells": {
-                        "rows": [ 
-                            {"values": [{"userEnteredValue": {"stringValue": f"{chosenExcuse}"}}]}
-                        ],
-                        "fields": "userEnteredValue",
-                        "range": {
-                            "sheetId": worksheetID,
-                            "startRowIndex": rowToFind, # First row
-                            "endRowIndex": rowToFind + 1,
-                            "startColumnIndex": columnToFind, # Column D
-                            "endColumnIndex": columnToFind + 1,
+        compiledRequests = []
+        for col in columnToFind:
+            ExcuseReq = { 
+                "requests": [
+                    {
+                        "updateCells": {
+                            "rows": [ 
+                                {"values": [{"userEnteredValue": {"stringValue": f"{chosenExcuse}"}}]}
+                            ],
+                            "fields": "userEnteredValue",
+                            "range": {
+                                "sheetId": worksheetID,
+                                "startRowIndex": rowToFind,
+                                "endRowIndex": rowToFind + 1,
+                                "startColumnIndex": col,
+                                "endColumnIndex": col + 1
+                            }
                         }
                     }
-                }
-            ]
-        }
-        compiledRequests.extend(ExcuseReq["requests"])
+                ]
+            }
+            compiledRequests.extend(ExcuseReq["requests"])
 
         # Update to Sheets
         try:
@@ -172,9 +181,9 @@ class LabelsMenu(discord.ui.Select):
         print(f"Excuse command executed in {commandEndTime - commandStartTime:.4f} seconds\n")
 
 class LabelsMenuView(discord.ui.View):
-    def __init__(self, userID: str, activity: str):
+    def __init__(self, userID: str, activities: list):
         super().__init__(timeout=60) # Vanishes after 60s
-        self.add_item(LabelsMenu(userID, activity))
+        self.add_item(LabelsMenu(userID, activities))
 
 class Excuses(commands.Cog):
     def __init__(self, bot):
@@ -200,9 +209,23 @@ class Excuses(commands.Cog):
         ]
 
     # TODO: FIX EXCUSE CONFLICT WITH CHECKIN. Excuse could overwrite a cell, when the user is supposed to be done for that day.
-    @app_commands.command(name="excusesmenu", description="Fills in today's cell with the selected excuse")
-    @app_commands.autocomplete(activity = excuse_autocomplete)
-    async def excuse(self, interaction: discord.Interaction, activity: str):
+    @app_commands.command(name="test_excusesmenu", description="Fills in today's cell with the selected excuse")
+    @app_commands.autocomplete(
+        activity1 = excuse_autocomplete,
+        activity2 = excuse_autocomplete,
+        activity3 = excuse_autocomplete,
+        activity4 = excuse_autocomplete,
+        activity5 = excuse_autocomplete
+    )
+    async def test_excuse(
+        self, 
+        interaction: discord.Interaction, 
+        activity1: str,
+        activity2: str = None,
+        activity3: str = None,
+        activity4: str = None,
+        activity5: str = None):
+
         userID = str(interaction.user.id)
         usersData = utls.loadJSON(CFG.USERS_FILE)
         try:
@@ -211,22 +234,34 @@ class Excuses(commands.Cog):
             print(f"{interaction.user} was not found")
         userActivities: list = user['activities']
 
-        if activity not in userActivities:
-            message = "The activity you've inputted doesn't match with your registered activit"
-            localMessage = f"{interaction.user} inputted: ({activity}) and it doesn't match with their registered activit"
-            if len(userActivities) == 1:
-                newLocalMessage = localMessage + 'y'
-                newMessage = message + 'y'
+        await interaction.response.defer()
+        # Filter the activities        
+        groupedAct: tuple = (activity1, activity2, activity3, activity4, activity5)
+        valid: list = []
+        invalid: list = []
+        for activity in groupedAct:
+            if activity is None or activity in valid: # Skip duplicates or None
+                continue
+            if activity in userActivities:
+                valid.append(activity)
             else:
-                newLocalMessage = localMessage + 'ies'
-                newMessage = message + 'ies'
-            print(newLocalMessage)
-            await interaction.response.send_message(newMessage)
+                invalid.append(activity)
+        print(f"{interaction.user} selected: {valid}")
+        if invalid:
+            await interaction.followup.send(f"{invalid} doesn't match with your registered activity!", ephemeral=True)
 
-        
-        await interaction.response.send_message(f"Select your excuse for today's {activity}", view = LabelsMenuView(userID, activity))
+        # Activities validation
+        if not valid:
+            if len(groupedAct) == 1:
+                await interaction.followup.send(f"The activity you've inputted doesn't match with your registered activity")
+                return 
+            else:
+                await interaction.followup.send(f"The activities you've inputted doesn't match with your registered activities")
+                return         
+
+        await interaction.followup.send(f"Select your excuse for today's {valid}", view = LabelsMenuView(userID, valid))        
+
 async def setup(bot: commands.Bot):
     _GUILD_ID = discord.Object(id = CFG.GUILD_ID)
     await bot.add_cog(Excuses(bot), guild = _GUILD_ID)
-
     
