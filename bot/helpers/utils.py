91b4059f8d4e11aec93af50377574d1d2ee3dae9
Cryptoz <jasondrake058@gmail.com>
2026-06-12 09:56:56 +0700

    Moved some functions from registration to utils

--- bot/helpers/utils.py
+++ bot/helpers/utils.py
@@ -37,6 +37,31 @@ def saveJSON(data, file_path):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent = 4)
 
+
+def newUserSheetID(userID: int):
+    """Hashed user sheetID I guess?"""
+    newSheetID = userID % 1_000_000_000
+    return newSheetID
+
+def templateSheetLayout(username: str, format: str): # All index are 0-based
+    FORMATS = { # These are the static table locations that are in "Template" sheet, don't use this after copying once
+        "Yearly": (0, 34, 3, 17),    # yearly | 3 -> D column | 17 -> R column (exclusive)        
+        "Semesterly_Standard": (35, 70, 3, 17),   # semester
+        "Semesterly_Extended": (35, 70, 18, 38),  # semester (alternate) | 18 -> S column | 38 is AM column (exclusive)
+        "Quarterly_Standard": (71, 106, 3, 17),  # quarter
+        "Quarterly_Extended": (71, 106, 18, 35), # quarter (alternate) | 18 -> S column | 35 is AJ column (exclusive)
+    }
+    startRow, endRow, startCol, endCol = FORMATS[format]
+    data = {
+        username: {
+            "startRowIndex": startRow,
+            "endRowIndex": endRow,
+            "startColumnIndex": startCol,
+            "endColumnIndex": endCol,
+        }
+    }
+    return data
+
 def semesterly_standard_selector(col):
     return 0 if col % 2 == 1 else 1
 
