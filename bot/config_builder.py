3e608d5fd5896e939a79f5cf1db407a8e6e9de4e
Cryptoz <jasondrake058@gmail.com>
2026-07-25 14:16:33 +0700

    Casted GUILD_ID into an int (it was str by default)

--- bot/config_builder.py
+++ bot/config_builder.py
@@ -15,14 +15,8 @@ config.read(BASE_DIR / "config.ini")
 class ConfigDTO:
     """Put the file configs into objects"""
     GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
-    GUILD_ID        = os.getenv("SERVER_ID")
+    GUILD_ID        = int(os.getenv("SERVER_ID"))
     CREDS           = json.loads(os.getenv("CREDS"))
     CHECKIN_FILE    = BASE_DIR / config["Paths"]["checkin_file"]
     USERS_FILE      = BASE_DIR / config["Paths"]["users_file"]
     SHEET_CACHE     = BASE_DIR / config["Paths"]["sheet_cache"]
-
-
-if __name__ == "__main__":
-    cfg = ConfigDTO()
-    print(cfg.CHECKIN_FILE)
-    print(cfg.SHEET_CACHE)
