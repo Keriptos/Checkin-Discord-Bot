0db22a72b01705069fa302b61297ba0361ac229c
Cryptoz <jasondrake058@gmail.com>
2026-09-20 21:15:15 +0700

    READDED THE BOT MESSAGE (I somehow deleted this...)

--- bot/cogs/check_in_outs.py
+++ bot/cogs/check_in_outs.py
@@ -180,7 +180,8 @@ class CheckinMenu(discord.ui.Select): # A menu to select your activities up to 5
                 processStartTime = time.perf_counter()                             
                 await (await SheetService.get_spreadsheet_client()).batch_update({"requests": compiledRequests}) 
                 processEndTime = time.perf_counter()
-                print(f"Sucessfully checked in user in {processEndTime - processStartTime:.4f} seconds")            
+                print(f"Sucessfully checked in user in {processEndTime - processStartTime:.4f} seconds")
+                await interaction.followup.send(f"{interaction.user.mention} has checked in to {'Supabase and 'if supa_checked_in else ''}Sheets for {', '.join(chosen)}")
         except Exception as error:
             print(f"An error has occured when batch-updatin, {error}\n")
 
